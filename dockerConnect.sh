b2c3312ece0b5ee7299a5aec1e18078b62d0ada7
tony <tony.haines@infinateblu.com>
2025-03-26 14:15:21 +0000

    Removed the docker, and converted to more data in arrays

--- dockerConnect.sh
+++
@@ -1,9 +0,0 @@
-# Get the container ID of the running zoho-api-test container
-container_id=$(sudo docker ps -q -f ancestor=amscreen-rdm-sim)
-
-# Connect to the running container for debugging
-if sudo docker exec -it $container_id /bin/bash; then
-  echo "Connected using /bin/bash"
-else
-  sudo docker exec -it $container_id /bin/sh
-fi
